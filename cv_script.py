93a50b623dcca82d9281fd472e1f0c13a5b9c7a8
Matus Tomlein <matus.tomlein@gmail.com>
2022-03-11 09:07:12 +0100

    Add CV script

---
+++ cv_script.py
@@ -0,0 +1,15 @@
+import os
+
+from dataset.DatasetUtils import DatasetUtils
+from classifier.featureengineering.FeatureEngineeringModels import FeatureEngineeringModels
+from classifier.training.ClassifierTraining import ClassifierTraining
+
+# Create Objects
+dataset = DatasetUtils()
+featureEngineeringModels = FeatureEngineeringModels(dataset_object=dataset)
+
+
+# Create ClassifierTraining Object
+classifierTrainingObject = ClassifierTraining(dataset_object=dataset)
+
+classifierTrainingObject.train_model()
