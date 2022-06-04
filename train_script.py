90ef81a2a27bafe591439b9a49e37c0bd1c51df5
Matus Tomlein <matus.tomlein@gmail.com>
2022-06-04 09:22:12 +0200

    Add updates to train_script

--- train_script.py
+++ train_script.py
@@ -8,6 +8,29 @@ from classifier.training.ClassifierTraining import ClassifierTraining
 dataset = DatasetUtils()
 featureEngineeringModels = FeatureEngineeringModels(dataset_object=dataset)
 
+# # Video Snippet
+# # Generate Video Snippet fastText input features
+# featureEngineeringModels.prepare_fasttext_data(model_type='video_snippet', overwrite=True)
+# # Fine-tune a fastText model for Video Snippet
+# featureEngineeringModels.finetune_model(model_type='video_snippet', overwrite=True)
+
+# # # Video Tags
+# # # Generate Video Tags fastText input features
+# # featureEngineeringModels.prepare_fasttext_data(model_type='video_tags')
+# # # Fine-tune a fastText model for Video Tags
+# # featureEngineeringModels.finetune_model(model_type='video_tags')
+
+# # Video Transcript
+# # Generate Video Transcript fastText input features
+# featureEngineeringModels.prepare_fasttext_data(model_type='video_transcript', overwrite=True)
+# # Fine-tune a fastText model for Video Transcript
+# featureEngineeringModels.finetune_model(model_type='video_transcript', overwrite=True)
+
+# # Video Comments
+# # Generate Video Comments fastText input features
+# featureEngineeringModels.prepare_fasttext_data(model_type='video_comments', overwrite=True)
+# # Fine-tune a fastText model for Video Comments
+# featureEngineeringModels.finetune_model(model_type='video_comments', overwrite=True)
 
 # Create ClassifierTraining Object
 classifierTrainingObject = ClassifierTraining(dataset_object=dataset)
